bbf31d74fb44385ed06f18a380bc8b79b5b26ea5
Shukree Abuwi <shushuhead@gmail.com>
2022-07-09 01:33:45 -0400

    Updates

---
+++ precache-manifest.cd4fa41e0f77c12c3ad2b2d3909def0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9924cdc617c6a340e27",
+    "url": "/rentassistanceapp/static/js/main.f9924cdc.chunk.js"
+  },
+  {
+    "revision": "c2993aa875e975957abf",
+    "url": "/rentassistanceapp/static/js/1.c2993aa8.chunk.js"
+  },
+  {
+    "revision": "05f4d8db12589c9406fb",
+    "url": "/rentassistanceapp/static/js/runtime~main.05f4d8db.js"
+  },
+  {
+    "revision": "3b35fbe897571b8597e1a99aba49ce87",
+    "url": "/rentassistanceapp/index.html"
+  }
+];
